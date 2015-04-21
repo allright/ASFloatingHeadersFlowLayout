d7106b10b23ff0c3ad0f9e6712a8250037e5db44
Andrey.Syvrachev <andrey.syvrachev@telum.ru>
2015-04-21 12:37:03 +0300

    DemoLayout

---
+++ FloatingHeadersDemo/FloatingHeadersDemo/GridViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  FloatingHeadersDemo
+//
+//  Created by Andrey Syvrachev on 17.04.15.
+//  Copyright (c) 2015 Andrey Syvrachev. All rights reserved.
+//
+
+import UIKit
+
+let CellReuseId = "Cell"
+let HeaderReuseId = "Header"
+let FooterReuseId = "Footer"
+
+class GridViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 100
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return section%20 + 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCellWithReuseIdentifier(CellReuseId, forIndexPath: indexPath) as! UICollectionViewCell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: kind == UICollectionElementKindSectionHeader ? HeaderReuseId : FooterReuseId, forIndexPath: indexPath) as! UICollectionReusableView
+    }
+}
+

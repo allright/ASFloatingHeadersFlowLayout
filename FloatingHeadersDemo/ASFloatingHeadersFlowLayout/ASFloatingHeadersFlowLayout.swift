240851fdfa8231a47aaba794181d4e0c8ab47924
Andrey.Syvrachev <andrey.syvrachev@telum.ru>
2015-04-23 18:28:20 +0300

    working version!

--- FloatingHeadersDemo/ASFloatingHeadersFlowLayout/ASFloatingHeadersFlowLayout.swift
+++ FloatingHeadersDemo/ASFloatingHeadersFlowLayout/ASFloatingHeadersFlowLayout.swift
@@ -13,16 +13,49 @@ class ASFloatingHeadersFlowLayout: UICollectionViewFlowLayout {
     var sectionHeadersAttributes:Array<UICollectionViewLayoutAttributes!> = []
     let offsets = NSMutableOrderedSet()
     var floatingSectionIndex:Int! = nil
+    var previousWidth:CGFloat! = nil
     
     override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
         return true
     }
     
+    func calculateHeaders(){
+        self.sectionHeadersAttributes.removeAll(keepCapacity: true)
+        self.offsets.removeAllObjects()
+        
+        let collectionView = self.collectionView!
+        
+        let numberOfSections = collectionView.numberOfSections()
+        for var section = 0; section < numberOfSections; ++section {
+            
+            let attr =  super.layoutAttributesForSupplementaryViewOfKind(UICollectionElementKindSectionHeader,atIndexPath:NSIndexPath(forItem: 0, inSection: section))
+            self.sectionHeadersAttributes.append(attr)
+            
+            if (section > 0){
+                self.offsets.addObject(attr.frame.origin.y)
+            }
+        }
+        
+
+    }
+    
+    func setOffsetOfFloatingHeader(){
+        let collectionView = self.collectionView!
+        let offset:CGFloat = collectionView.contentOffset.y + collectionView.contentInset.top
+        let index = indexForOffset(offset)
+        self.setFloatingHeaderOffset(offset, forIndex: index)
+    }
+    
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
+
+        println("layoutAttributesForElementsInRect \(rect) ... ")
+        println("1contentInset.top = \(self.collectionView!.contentInset.top) .....")
+        
+        setOffsetOfFloatingHeader()
 
         let attrs = super.layoutAttributesForElementsInRect(rect)
         
-        return attrs?.map() {
+        let ret = attrs?.map() {
             
             (attribute) -> UICollectionViewLayoutAttributes in
             
@@ -36,11 +69,17 @@ class ASFloatingHeadersFlowLayout: UICollectionViewFlowLayout {
             
             return attr
         }
+        
+        println("layoutAttributesForElementsInRect \(rect) ... OK")
+        return ret
     }
     
     override func layoutAttributesForSupplementaryViewOfKind(elementKind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
         
+        println("2contentInset.top = \(self.collectionView!.contentInset.top)")
+
         if (elementKind == UICollectionElementKindSectionHeader){
+            setOffsetOfFloatingHeader()
             return self.sectionHeadersAttributes[indexPath.section]
         }
         return super.layoutAttributesForSupplementaryViewOfKind(elementKind,atIndexPath:indexPath)
@@ -63,24 +102,37 @@ class ASFloatingHeadersFlowLayout: UICollectionViewFlowLayout {
         let attrs = self.sectionHeadersAttributes[forIndex]
         attrs.frame = CGRectMake(0, offset, attrs.frame.size.width, attrs.frame.size.height)
         attrs.zIndex = 1024
+        println("SetOffset: \(offset) forIndex:\(forIndex)")
     }
+    
+//    override func targetContentOffsetForProposedContentOffset(proposedContentOffset: CGPoint) -> CGPoint {
+//        <#code#>
+//    }
     
     override func invalidationContextForBoundsChange(newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
         
-//        println("invalidationContextForBoundsChange(\(newBounds))")
         
         var context = super.invalidationContextForBoundsChange(newBounds)
+        if let width = self.previousWidth{
+            if (width != newBounds.size.width){
+                println("FORCE PREPARE LAYOUT width changed \(width) -> \(newBounds.size.width) newBounds = \(newBounds)")
+                self.previousWidth = newBounds.size.width
+                return context
+            }
+        }
+        
+        self.previousWidth = newBounds.size.width
 
         // здесь инвалидируем конкретный аттрибут - выставляем его новые значения!,
         let collectionView = self.collectionView!
         
         let offset:CGFloat = newBounds.origin.y + collectionView.contentInset.top
         let index = indexForOffset(offset)
+  //      self.setFloatingHeaderOffset(offset, forIndex: index)
         
+
         var invalidatedIndexPaths = [NSIndexPath(forItem: 0, inSection:index)];
-        
-        self.setFloatingHeaderOffset(offset, forIndex: index)
-        
+
         if let floatingSectionIndex = self.floatingSectionIndex {
             if (self.floatingSectionIndex != index){
                 invalidatedIndexPaths.append(NSIndexPath(forItem: 0, inSection:floatingSectionIndex))
@@ -90,6 +142,9 @@ class ASFloatingHeadersFlowLayout: UICollectionViewFlowLayout {
         
         context.invalidateSupplementaryElementsOfKind(UICollectionElementKindSectionHeader,atIndexPaths:invalidatedIndexPaths)
 
+        println("invalidationContextForBoundsChange(\(newBounds)) \(invalidatedIndexPaths) contentInset.top = \(collectionView.contentInset.top)")
+
+        
         return context
     }
     
@@ -100,29 +155,22 @@ class ASFloatingHeadersFlowLayout: UICollectionViewFlowLayout {
 
         
         super.prepareLayout()
-        println("prepareLayout")
+        println("prepareLayout ...")
 
-        self.sectionHeadersAttributes.removeAll(keepCapacity: true)
-        self.offsets.removeAllObjects()
+        calculateHeaders()
+
         
-        let cv = self.collectionView!
-        
-        let numberOfSections = cv.numberOfSections()
-        for var section = 0; section < numberOfSections; ++section {
-            
-            let attr =  super.layoutAttributesForSupplementaryViewOfKind(UICollectionElementKindSectionHeader,atIndexPath:NSIndexPath(forItem: 0, inSection: section))
-            self.sectionHeadersAttributes.append(attr)
-            
-            if (section > 0){
-                self.offsets.addObject(attr.frame.origin.y)
-            }
-        }
+//        
+//        let offset:CGFloat = collectionView.contentOffset.y + collectionView.contentInset.top
+//        let index = indexForOffset(offset)
+//        self.setFloatingHeaderOffset(offset, forIndex: index)
+//        self.floatingSectionIndex = index
         
         // ПОДУМАТЬ -> тут добавить еще пересчет первого видимого!
         
         
         let stop = CFAbsoluteTimeGetCurrent()
-        println("init time = \(stop - start) ")
+        println("prepareLayout ... done in \(stop - start) sec")
     }
     
 }
